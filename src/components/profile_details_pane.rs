4f9282b6bae9cf4b75b0d4da67f74fe8d3a6487b
natix1 <natix1.contact@gmail.com>
2026-02-27 12:23:27 +0100

    Add profile version top display

--- src/components/profile_details_pane.rs
+++ src/components/profile_details_pane.rs
@@ -8,12 +8,13 @@ use crate::{
 #[component]
 pub fn ProfileDetailsPane() -> Element {
     let profile_store = use_context::<ProfileStore>();
-    if (*profile_store.selected_profile_name.read()).is_some() {
+    if let Some(selected_name) = &*profile_store.selected_profile_name.read() {
         rsx! {
             div {
                 class: "w-5/6 flex flex-col bg-white/5 rounded-xl h-full p-4 gap-3 ring-1 ring-white/15",
                 h1 {
                     class: "w-full text-center text-4xl font-bold",
+                    {format!("{}", selected_name)}
                 }
                 div {
                     class: "w-full flex-shrink-0",
